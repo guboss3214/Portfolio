9d884758e9cf5f3279647aea12a1721b662806c5
Yur@Fiv3 <uraporskalo@gmail.com>
2024-11-13 21:16:12 +0200

    Added some style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.querySelector('.header-navbar__btn');
-  const list = document.querySelector('.header-navbar__list');
-
-  if (!btn || !list) return;
-
-  btn.addEventListener('click', () => {
-    btn.classList.toggle('header-navbar__btn--active');
-
-    if (btn.classList.contains('header-navbar__btn--active')) {
-      list.classList.add('animate__flipInX');
-      list.classList.remove('animate__flipOutX');
-    } else {
-      list.classList.remove('animate__flipInX');
-      list.classList.add('animate__flipOutX');
-    }
-  });
-
+  const burger = document.querySelector('#burger')
+  const span = burger.querySelectorAll('span')
+  const menu = document.querySelector('.navbar__menu')
+  span.forEach(el => el.addEventListener('click', () => {
+    burger.classList.toggle('active')
+    menu.classList.toggle('active')
+  }));
 });
+
+document.addEventListener('scroll',(e) => {
+  const e = e.target
+})
