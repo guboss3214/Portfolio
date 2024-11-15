48e899eb52c7a19d470cd7a45907fa1ab781e496
Yur@Fiv3 <uraporskalo@gmail.com>
2024-11-15 14:27:56 +0200

    Some fixing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,15 @@
+const projectSection = document.querySelector('#projects')
+const skillsSection = document.querySelector('#skills')
+const footerSection = document.querySelector('#footer')
+
 document.addEventListener('DOMContentLoaded', () => {
   const burger = document.querySelector('#burger')
   const span = burger.querySelectorAll('span')
   const menu = document.querySelector('.navbar__menu')
+
   span.forEach(el => el.addEventListener('click', () => {
     burger.classList.toggle('active')
     menu.classList.toggle('active')
   }));
+  
 });
-
-document.addEventListener('scroll',(e) => {
-  const e = e.target
-})
